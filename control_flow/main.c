8a3700bba26afa1862ea0295151454e10f7f7b51
bps <simon.f.kern@gmail.com>
2025-01-26 18:03:05 +0100

    thefk

---
+++ control_flow/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+
+
+/* binsearch: find x in v[0] <= v[1] <= ... <=[n-1]*/
+int binsearch(int x, int v[], int n);
+
+int main()
+{
+	int arr[] = {1, 4, 9};
+	
+	printf("%d", binsearch(4, arr, 3));
+
+	return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+	int low, high, mid;
+	low = 0;
+	high = n - 1;
+	while (low <= high){
+		mid = (low+high)/2;
+		if (x < v[mid])
+			high = mid + 1;
+		else if ( x > v[mid])
+			low = mid + 1;
+		else return mid;
+	}
+
+	return -1;
+
+}
